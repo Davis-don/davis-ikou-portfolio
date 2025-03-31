9cf26b0fdc711aba3f9b05da5ca617587a8d44cc
Davis-don <davismugoikou@gmail.com>
2025-03-31 18:14:30 +0300

    added welcome section

---
+++ src/Homepagewelcome/Welcomesection.jsx
@@ -0,0 +1,13 @@
+import './welcomesection.css'
+
+function Welcomesection() {
+  return (
+    <div className='overerall-welcome-section-container'>
+       <h1>Welcome To The Official Site Of Davis Ikou</h1>
+       <p className='step-into-paragraph'>Step into the world of Davis Ikou</p>
+       <p className='discover-paragraph'>Discover the journey, innovations, and groundbreaking contributions of one of the world’s leading software engineers.</p>
+        </div>
+  )
+}
+
+export default Welcomesection
--- src/Homescreen.jsx
+++ src/Homescreen.jsx
@@ -1,10 +1,12 @@
 import './homescreen.css'
 import Hero from './components/Homepage Hero/Hero'
+import Welcomesection from './Homepagewelcome/Welcomesection'
 
 function Homescreen() {
   return (
     <div className='overall-homescreen-container'>
       <Hero/>
+      <Welcomesection/>
         </div>
   )
 }
